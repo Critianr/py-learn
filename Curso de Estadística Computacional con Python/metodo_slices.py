5075193ea7dfbc29ef661bef28cbedd1bad59d93
unknown <calrod350@gmail.com>
2022-04-24 18:40:09 -0500

    two

---
+++ Curso de Estadística Computacional con Python/metodo_slices.py
@@ -0,0 +1,15 @@
+nombre = "Lederman"
+nombre[0]
+# 'L'
+nombre[0:3]
+# 'Led'
+nombre[:3]
+# 'Led'
+nombre[3:0]
+# 'erman'
+nombre[1:7]
+# 'ederma'
+nombre[1:7:2]
+# 'eem
+nombre[::]
+# 'Lederman'
---
+++ Curso de Estadística Computacional con Python/num-cuatro-cifras.py
@@ -0,0 +1,24 @@
+num = input('Digite un número de cuatro cifras: ')
+
+# while len(len):
+if num[0] == '-' :
+    if(num[1] == '1'):
+        print('salir del programa')
+    else:
+        if num[1] == '-':
+            if num[2] == '1':
+                print('sale del programa')
+else:
+    print('se queda en el programa')
+
+
+
+
+        
+
+
+    
+    
+    
+    
+# print (num[0])
